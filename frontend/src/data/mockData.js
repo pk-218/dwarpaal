42498ed534c053fcf6a54007171644e6ac29ff79
dhanishtaghosh <dhanishthaghosh@gmail.com>
2023-01-14 03:14:21 +0530

    UI request form + admin dashboard

---
+++ frontend/src/data/mockData.js
@@ -0,0 +1,173 @@
+export const mockTransactions = [
+    {
+        txId: "01e4dsa",
+        user: "johndoe",
+        date: "2021-09-01",
+        cost: "43.95",
+    },
+    {
+        txId: "0315dsaa",
+        user: "jackdower",
+        date: "2022-04-01",
+        cost: "133.45",
+    },
+    {
+        txId: "01e4dsa",
+        user: "aberdohnny",
+        date: "2021-09-01",
+        cost: "43.95",
+    },
+    {
+        txId: "51034szv",
+        user: "goodmanave",
+        date: "2022-11-05",
+        cost: "200.95",
+    },
+    {
+        txId: "0a123sb",
+        user: "stevebower",
+        date: "2022-11-02",
+        cost: "13.55",
+    },
+    {
+        txId: "01e4dsa",
+        user: "aberdohnny",
+        date: "2021-09-01",
+        cost: "43.95",
+    },
+    {
+        txId: "120s51a",
+        user: "wootzifer",
+        date: "2019-04-15",
+        cost: "24.20",
+    },
+    {
+        txId: "0315dsaa",
+        user: "jackdower",
+        date: "2022-04-01",
+        cost: "133.45",
+    },
+];
+
+export const mockDataContacts = [
+    {
+        id: 1,
+        username: "Jon Snow",
+        validity: "01/03/2023",
+        revoked: false,
+        registrarId: 123512,
+    },
+    {
+        id: 2,
+        username: "Cersei Lannister",
+        validity: "03/03/2023",
+        revoked: false,
+        registrarId: 123512,
+    },
+    {
+        id: 3,
+        username: "Jaime Lannister",
+        validity: "05/03/2023",
+        revoked: false,
+        registrarId: 4132513,
+    },
+    {
+        id: 4,
+        username: "Anya Stark",
+        validity: "07/03/2023",
+        revoked: false,
+        registrarId: 123512,
+    },
+    {
+        id: 5,
+        username: "Daenerys Targaryen",
+        validity: "09/03/2023",
+        revoked: false,
+        registrarId: 123512,
+    },
+    {
+        id: 6,
+        username: "Ever Melisandre",
+        validity: "11/03/2023",
+        revoked: false,
+        registrarId: 123512,
+    },
+    {
+        id: 7,
+        username: "Ferrara Clifford",
+        validity: "13/03/2023",
+        revoked: false,
+        registrarId: 123512,
+    },
+    {
+        id: 8,
+        username: "Rossini Frances",
+        validity: "15/03/2023",
+        revoked: false,
+        registrarId: 512315,
+    },
+    {
+        id: 9,
+        username: "Harvey Roxie",
+        validity: "17/03/2023",
+        revoked: false,
+        registrarId: 928397,
+    },
+];
+
+export const mockLineData = [
+    {
+        id: "memory",
+        color: "#fdde6c",
+        data: [
+            {
+                x: "plane",
+                y: 101,
+            },
+            {
+                x: "helicopter",
+                y: 75,
+            },
+            {
+                x: "boat",
+                y: 36,
+            },
+            {
+                x: "train",
+                y: 216,
+            },
+            {
+                x: "subway",
+                y: 35,
+            },
+            {
+                x: "bus",
+                y: 236,
+            },
+            {
+                x: "car",
+                y: 88,
+            },
+            {
+                x: "moto",
+                y: 232,
+            },
+            {
+                x: "bicycle",
+                y: 281,
+            },
+            {
+                x: "horse",
+                y: 1,
+            },
+            {
+                x: "skateboard",
+                y: 35,
+            },
+            {
+                x: "others",
+                y: 14,
+            },
+        ],
+    }
+];
