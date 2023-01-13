ad6fe063d12a192d398891c878387467ab34eb34
Pankaj Khushalani <2182001pk@gmail.com>
2023-01-14 03:24:56 +0530

    feat: revoke user access
    Signed-off-by: Pankaj Khushalani <2182001pk@gmail.com>

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,11 +1,12 @@
-import { Router } from "express"
-import Admin from "../controllers/admin.js"
+import { Router } from "express";
+import Admin from "../controllers/admin.js";
 
-const adminRouter = Router()
+const adminRouter = Router();
 
-adminRouter.get("/get-all-users",Admin.getAllUsers)
-adminRouter.get("/get-logged-in-users",Admin.getLoggedInUsers)
-adminRouter.get("/memory-usage-per-user",Admin.memoryUsagePerUser)
-adminRouter.post("/create-user",Admin.createUser)
+adminRouter.get("/get-all-users", Admin.getAllUsers);
+adminRouter.get("/get-logged-in-users", Admin.getLoggedInUsers);
+adminRouter.get("/memory-usage-per-user", Admin.memoryUsagePerUser);
+adminRouter.post("/create-user", Admin.createUser);
+adminRouter.post("/delete-user", Admin.deleteUser);
 
-export default adminRouter
+export default adminRouter;
